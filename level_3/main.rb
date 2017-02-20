9e135b68a738773e333d1947e675a345e1053ea7
rfkdali <rfkdali@gmail.com>
2017-02-20 13:35:19 +0100

    Replace title by gender, ability to use #female? or #male? method

--- level_3/main.rb
+++ level_3/main.rb
@@ -1,5 +1,18 @@
 require_relative "person"
+require "csv"
 
 source_path = File.join(__dir__, "source.csv")
+file = CSV.read(source_path)
 
 # it's up to you now!
+file.each do |row|
+  person = Person.new(row[0], row[1], row[2], row[3])
+  
+  # test output of .male? .female? method
+  p "MALE" if person.male?
+  p "FEMALE" if person.female?
+
+  person.infos
+end
+
+file
--- level_3/person.rb
+++ level_3/person.rb
@@ -1,1 +1,27 @@
 # Here should be the `Person` class
+
+class Person
+
+  def initialize gender, first_name, last_name, email
+    @gender = gender
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+  end
+
+  def male?
+    @gender == "M"
+  end
+
+  def female?
+    @gender == ("Mme" || "Mlle")
+  end
+
+  def infos
+    puts "gender: #{@gender}\n"
+    puts "first_name: #{@first_name}\n"
+    puts "last_name: #{@last_name}\n"
+    puts "email: #{@email}\n"
+    puts "\n"
+  end
+end
