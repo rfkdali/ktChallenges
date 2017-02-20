ce467ad3994e1bb9a5e859e3f87ce4aa5f3186d7
rfkdali <rfkdali@gmail.com>
2017-02-20 13:17:58 +0100

    Ability to return instances of the `Person` class accordingly to the csv file

--- level_2/main.rb
+++ level_2/main.rb
@@ -1,5 +1,12 @@
 require_relative "person"
+require "csv"
 
 source_path = File.join(__dir__, "source.csv")
+file = CSV.read(source_path)
 
-# it's up to you now!
+file.each do |row|
+  person = Person.new(row[0], row[1], row[2], row[3])
+  person.infos
+end
+
+file
--- level_2/person.rb
+++ level_2/person.rb
@@ -1,13 +1,20 @@
 # Here should be the `Person` class
 
 class Person
-  attr_accessor :title, :first_name, :last_name, :email
 
-  def initialize
+  def initialize title, first_name, last_name, email
     @title = title
     @first_name = first_name
     @last_name = last_name
     @email = email
+  end
+
+  def infos
+    puts "title: #{@title}\n"
+    puts "first_name: #{@first_name}\n"
+    puts "last_name: #{@last_name}\n"
+    puts "email: #{@email}\n"
+    puts "\n"
   end
 
 end
