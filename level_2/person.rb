c91e7906967a6243fd13b506bd56b0be3e90f968
rfkdali <rfkdali@gmail.com>
2017-02-20 12:50:27 +0100

    Ability to instanciate new Person with the specified attributes

--- level_2/person.rb
+++ level_2/person.rb
@@ -1,1 +1,13 @@
 # Here should be the `Person` class
+
+class Person
+  attr_accessor :title, :first_name, :last_name, :email
+
+  def initialize
+    @title = title
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+  end
+
+end
